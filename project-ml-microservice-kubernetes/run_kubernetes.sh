0299d8d9e04c69cf359847f430aba83f4bbfe4cb
sharpbm <pankaj.sharma1@natwest.com>
2022-10-15 04:50:10 +0000

    task6

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="pankaj765/mlmicroservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mlmicroservice --image=$dockerpath --port=80 --labels app=mlmicroservice
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get po
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlmicroservice 8000:80
 
